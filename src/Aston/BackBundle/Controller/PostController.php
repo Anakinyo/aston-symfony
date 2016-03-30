718a5f7b9751a0c68e3b44bcbfa5a176aecd1531
Yoann BELOT <belot.yoann@hotmail.fr>
2016-03-30 12:32:30 +0200

    maj post controller

---
+++ src/Aston/BackBundle/Controller/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Aston\BackBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Description of Post
+ *
+ * @author yoann
+ */
+class PostController extends Controller
+{
+
+  public function listAction()
+  {
+    
+  }
+
+  public function addAction(Request $req)
+  {
+    
+  }
+  
+  public function updateAction(Request $req)
+  {
+    
+  }
+
+  public function deleteAction(Request $req)
+  {
+    
+  }
+}
